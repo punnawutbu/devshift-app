69456cbd2124aebc82ca764c61ca73ab357c9e8e
punnawut bucha <punnawut_bucha@outlook.com>
2024-09-11 21:07:16 +0700

    Feat : Fix server.js

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-const { createServer } = require('http');
+const express = require('express');
 const next = require('next');
 
+const port = process.env.PORT || 4001;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const port = process.env.PORT || 4001;
-
 app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, (err) => {
+  const server = express();
+
+  // Example of custom route
+  server.get('/custom-route', (req, res) => {
+    return app.render(req, res, '/custom-page', req.query);
+  });
+
+  // Handle all other routes with Next.js
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
