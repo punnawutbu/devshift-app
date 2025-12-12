d1f7449c7b81572e3457713409e110462da3719f
punnawut bucha <punnawut_bucha@outlook.com>
2025-12-12 14:00:34 +0700

    ⚡️ Fix : fetch api

--- src/app/gold-price/page.tsx
+++ src/app/gold-price/page.tsx
@@ -111,8 +111,8 @@ const GoldPricePage: React.FC = () => {
   useEffect(() => {
     const fetchGoldPrice = async () => {
       try {
-        const response = await fetch('/api/gold-price');
-        // const response = await fetch('/.netlify/functions/getGoldPrice');
+        // const response = await fetch('/api/gold-price');
+        const response = await fetch('/.netlify/functions/getGoldPrice');
         const data: GoldPrice = await response.json();
         setGoldPrice(data);
       } catch (error) {
